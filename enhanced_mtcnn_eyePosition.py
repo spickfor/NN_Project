7dd153996233aee104eba2f894d47da2345b6326
S Pickford <spickford@ss-mbp-2.dhcp.nd.edu>
2024-05-05 18:17:38 -0400

    added test database photo

--- enhanced_mtcnn_eyePosition.py
+++ enhanced_mtcnn_eyePosition.py
@@ -48,9 +48,10 @@ def load_model(model_path):
 detector = MTCNN()
 
 # Load an image
-image_path = 'BioID_0000.pgm'
-# image_path = 'BioID_0016 copy.pgm'
+# image_path = 'BioID_0000.pgm'         # from validation set
+# image_path = 'BioID_0016 copy.pgm'    # from training set
 # image_path = 'resized_test.jpeg'
+image_path = 's08_01.jpg'               # from test dataset
 
 image = cv2.imread(image_path)
 rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
